52f6c0bf24cd11f8096b5f152f45f0b281eb62fb
unknown <emorris@network-03.CASTLEROCK.local>
2014-02-10 09:21:35 -0700

    Add test functionality for securo error

--- web.js
+++ web.js
@@ -36,6 +36,42 @@ app.get('/', function(req, res){
 	res.end();
 });
 
+//SECURO ERROR
+app.get('/securo', function(req, res){
+  var nodemailer = require("nodemailer");
+
+	// create reusable transport method (opens pool of SMTP connections)
+	var smtpTransport = nodemailer.createTransport("SMTP",{
+    	service: "Gmail",
+    	auth: {
+        	user: "solaroerror@crri.co.in",
+        	pass: "3xt3rm1n80r"
+    	}
+	});
+
+	var mailOptions = {
+    	from: "Solaro Error<solaroerror@castlerockresearch.com>", // sender address
+    	to: emorris@castlerockresearch.com",
+		//to: "railsbangalore@crri.co.in,it-team@castlerockresearch.com", // list of receivers
+    	subject: "Securo Error Page Hit", // Subject line
+    	text: "Securo Error Page Hit", // plaintext body
+    	html: "<b>Securo error page has been hit.</b>" // html body
+	}
+	
+	smtpTransport.sendMail(mailOptions, function(error, res){
+    	if(error){
+        	console.log(error);
+    	}else{
+        	console.log("Message sent: " + res.message);
+   			}
+    	smtpTransport.close();
+	});
+		
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
+	res.end();
+});
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
