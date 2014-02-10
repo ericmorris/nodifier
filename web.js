4210c88d1372fb4c9965a35b9852245bf524adfb
unknown <emorris@network-03.CASTLEROCK.local>
2014-02-10 10:18:24 -0700

    Add Securo and solaro-in-brochure reporting

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 //SOLARO
-app.get('/', function(req, res){
+app.get('/solaro', function(req, res){
   var nodemailer = require("nodemailer");
 
 	// create reusable transport method (opens pool of SMTP connections)
@@ -32,7 +32,7 @@ app.get('/', function(req, res){
 	});
 		
 	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write(" A Solaro application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
+	res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
 	res.end();
 });
 
@@ -92,6 +92,76 @@ app.get('/solaro-in-brochure', function(req, res){
     	subject: "Solaro-in-brochure Error Page has been accessed", // Subject line
     	text: "Solaro-in-brochure Error Page has been accessed", // plaintext body
     	html: "<b>Solaro-in-brochure error page has been accessed.</b>" // html body
+	}
+	
+	smtpTransport.sendMail(mailOptions, function(error, res){
+    	if(error){
+        	console.log(error);
+    	}else{
+        	console.log("Message sent: " + res.message);
+   			}
+    	smtpTransport.close();
+	});
+		
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
+	res.end();
+});
+
+//SOLARO MAINTENANCE
+app.get('/maintenance_solaro', function(req, res){
+  var nodemailer = require("nodemailer");
+
+	// create reusable transport method (opens pool of SMTP connections)
+	var smtpTransport = nodemailer.createTransport("SMTP",{
+    	service: "Gmail",
+    	auth: {
+        	user: "solaroerror@crri.co.in",
+        	pass: "3xt3rm1n80r"
+    	}
+	});
+
+	var mailOptions = {
+    	from: "Solaro Error<solaroerror@castlerockresearch.com>", // sender address
+    	to: "railsbangalore@crri.co.in,it-team@castlerockresearch.com", // list of receivers
+    	subject: "Solaro Maintenance Page has been accessed", // Subject line
+    	text: "Solaro Maintenance Page has been accessed", // plaintext body
+    	html: "<b>Solaro Maintenance page has been hit.</b>" // html body
+	}
+	
+	smtpTransport.sendMail(mailOptions, function(error, res){
+    	if(error){
+        	console.log(error);
+    	}else{
+        	console.log("Message sent: " + res.message);
+   			}
+    	smtpTransport.close();
+	});
+		
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
+	res.end();
+});
+
+//SECURO MAINTENANCE
+app.get('/maintenance_securo', function(req, res){
+  var nodemailer = require("nodemailer");
+
+	// create reusable transport method (opens pool of SMTP connections)
+	var smtpTransport = nodemailer.createTransport("SMTP",{
+    	service: "Gmail",
+    	auth: {
+        	user: "solaroerror@crri.co.in",
+        	pass: "3xt3rm1n80r"
+    	}
+	});
+
+	var mailOptions = {
+    	from: "Solaro Error<solaroerror@castlerockresearch.com>", // sender address
+    	to: "railsbangalore@crri.co.in,it-team@castlerockresearch.com", // list of receivers
+    	subject: "Securo Maintenance Page has been accessed", // Subject line
+    	text: "Securo Maintenance Page has been accessed", // plaintext body
+    	html: "<b>Securo Maintenance page has been accessed.</b>" // html body
 	}
 	
 	smtpTransport.sendMail(mailOptions, function(error, res){
