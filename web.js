1e497e113b7d3d25eff22575d73af6ecf2a7420d
unknown <emorris@network-03.CASTLEROCK.local>
2014-02-14 14:35:45 -0700

    Fancier HTML

--- web.js
+++ web.js
@@ -106,11 +106,12 @@ app.get('/securo', function(req, res){
     	smtpTransport.close();
 	});
 		
-	//res.writeHead(200, {"Content-Type": "text/plain"});
-	//res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
-	//res.end();
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
+	res.end();
 	
-	res.writeHead(200, {'Content-Type': 'text/html'});
+	
+	/*res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<!doctype html>\n<html lang="en">\n' +
 	'<head>\n<meta charset="utf-8">\n<title>Request Timed Out</title>\n' +
 	'<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
@@ -119,6 +120,7 @@ app.get('/securo', function(req, res){
 	'\n</body>\n</html>');
 
 	res.end();
+	*/
 });
 
 //3.SOLARO-IN-BROCHURE ERROR
@@ -153,10 +155,11 @@ app.get('/solaro-in-brochure', function(req, res){
     	smtpTransport.close();
 	});
 		
-	//res.writeHead(200, {"Content-Type": "text/plain"});
-	//res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
-	//res.end();
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
+	res.end();
 	
+	/*
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<!doctype html>\n<html lang="en">\n' +
 	'<head>\n<meta charset="utf-8">\n<title>Request Timed Out</title>\n' +
@@ -166,6 +169,7 @@ app.get('/solaro-in-brochure', function(req, res){
 	'\n</body>\n</html>');
 
 	res.end();
+	*/
 });
 
 //4.SOLARO MAINTENANCE
@@ -199,10 +203,11 @@ app.get('/maintenance_solaro', function(req, res){
     	smtpTransport.close();
 	});
 		
-	//res.writeHead(200, {"Content-Type": "text/plain"});
-	//res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
-	//res.end();
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
+	res.end();
 	
+	/*
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<!doctype html>\n<html lang="en">\n' +
 	'<head>\n<meta charset="utf-8">\n<title>Request Timed Out</title>\n' +
@@ -212,6 +217,7 @@ app.get('/maintenance_solaro', function(req, res){
 	'\n</body>\n</html>');
 
 	res.end();
+	*/
 });
 
 //5.SECURO MAINTENANCE
@@ -246,10 +252,11 @@ app.get('/maintenance_securo', function(req, res){
     	smtpTransport.close();
 	});
 		
-	//res.writeHead(200, {"Content-Type": "text/plain"});
-	//res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
-	//res.end();
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write(" An application error has occurred while processing your request.\n Please press the back button and try again. \n\n We have been notified and are working on a solution to this error. \n\n Thank you.");
+	res.end();
 	
+	/*
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<!doctype html>\n<html lang="en">\n' +
 	'<head>\n<meta charset="utf-8">\n<title>Request Timed Out</title>\n' +
@@ -259,6 +266,7 @@ app.get('/maintenance_securo', function(req, res){
 	'\n</body>\n</html>');
 
 	res.end();
+	*/
 });
 });
 
