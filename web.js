2650e6af24252814d98a92276788d867afe12f02
unknown <emorris@network-03.CASTLEROCK.local>
2014-02-14 14:49:40 -0700

    Fancier HTML3

--- web.js
+++ web.js
@@ -1,32 +1,6 @@
 var express = require("express");
 var app = express();
 
-//datestamp
-/*
-function getDateTime() {
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return year + ":" + month + ":" + day + " at " + hour + ":" + min + ":" + sec;
-}
-*/
-
 //1.SOLARO
 app.get('/solaro', function(req, res){
   var nodemailer = require("nodemailer");
